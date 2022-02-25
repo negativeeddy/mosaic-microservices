288c4961ab4734dfe8f479f7662c0101b1ec1a3c
Ben Williams <ben@negativeeddy.com>
2022-02-25 14:41:41 -0600

    update deploy scripts

--- deploy/powershell/update_container_apps.ps1
+++ deploy/powershell/update_container_apps.ps1
@@ -1,9 +1,11 @@
 Param(
-    [string] $RESOURCE_GROUP,
     [string] $APP_NAME,
-    [string] $STAGE
+    [string] $STAGE,
+    [string] $IMAGE_TAG
 )
 
-az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-frontend-$STAGE" --image "mosaicprod.azurecr.io/mosaicfrontend:latest"
-az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-tilesapi-$STAGE" --image "mosaicprod.azurecr.io/mosaictilesapi:latest"
-az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-tileprocessor-$STAGE" --image "mosaicprod.azurecr.io/mosaictileprocessor:latest"
+$RESOURCE_GROUP = "$APP_NAME-rg-$STAGE"
+
+az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-frontend-$STAGE" --image "mosaicprod.azurecr.io/mosaicfrontend:$IMAGE_TAG"
+az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-tilesapi-$STAGE" --image "mosaicprod.azurecr.io/mosaictilesapi:$IMAGE_TAG"
+az containerapp update --resource-group $RESOURCE_GROUP --name "$APP_NAME-tileprocessor-$STAGE" --image "mosaicprod.azurecr.io/mosaictileprocessor:$IMAGE_TAG"
