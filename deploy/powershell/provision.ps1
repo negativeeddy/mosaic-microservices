73c4c227d8693436716b25bdfeaa0ea530da819d
Ben Williams <ben@negativeeddy.com>
2022-03-02 18:29:03 -0600

    add app insights to the solution

--- deploy/powershell/provision.ps1
+++ deploy/powershell/provision.ps1
@@ -10,6 +10,7 @@ Param(
 $RESOURCE_GROUP_NAME = "$APPNAME_BASE-rg-$STAGE"
 $CONTAINERAPPS_ENVIRONMENT_NAME = "${APPNAME_BASE}-env-${STAGE}"
 $LOG_ANALYTICS_WORKSPACE_NAME = "${APPNAME_BASE}-logs-${STAGE}"
+$APP_INSIGHTS_NAME = "${APPNAME_BASE}-appinsights-${STAGE}"
 $STORAGE_ACCOUNT_NAME = "${APPNAME_BASE}stg${STAGE}"
 
 $FRONTEND_NAME = "frontend"
@@ -41,6 +42,15 @@ Write-Output $LOG_ANALYTICS_WORKSPACE.ID
 $LOG_ANALYTICS_WORKSPACE_CLIENT_ID = (az monitor log-analytics workspace show --query customerId -g $RESOURCE_GROUP_NAME -n $LOG_ANALYTICS_WORKSPACE_NAME --out tsv)
 $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET = (az monitor log-analytics workspace get-shared-keys --query primarySharedKey -g $RESOURCE_GROUP_NAME -n $LOG_ANALYTICS_WORKSPACE_NAME --out tsv)
 
+# create app insights component
+Write-Output "creating app insights component"
+$APP_INSIGHTS = az monitor app-insights component create `
+  --app $APP_INSIGHTS_NAME `
+  --location $LOCATION `
+  --resource-group $RESOURCE_GROUP_NAME `
+  --workspace $LOG_ANALYTICS_WORKSPACE_NAME `
+  | ConvertFrom-Json
+
 # create the environment
 Write-Output "setting up container app environment" 
 
@@ -50,6 +60,7 @@ $CONTAINER_APP_ENVIRONMENT = az containerapp env create `
   --logs-workspace-id $LOG_ANALYTICS_WORKSPACE_CLIENT_ID `
   --logs-workspace-key $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET `
   --location $LOCATION `
+  --instrumentation-key $APP_INSIGHTS.instrumentationKey `
   | ConvertFrom-Json
 
 write-output $CONTAINER_APP_ENVIRONMENT.ID
@@ -94,7 +105,7 @@ $FRONTEND_APP = `
   --dapr-app-port 80 `
   --dapr-app-id $FRONTEND_NAME `
   --dapr-components './frontend-components.yaml' `
-  --secrets "storage-account-name=${STORAGE_ACCOUNT_NAME},storage-account-key=${STORAGE_ACCOUNT_KEY}"
+  --secrets "storage-account-name=${STORAGE_ACCOUNT_NAME},storage-account-key=${STORAGE_ACCOUNT_KEY}" `
   | ConvertFrom-Json
   
   Write-Output $FRONTEND_APP.ID
@@ -117,7 +128,7 @@ $FRONTEND_APP = `
   --dapr-app-id $TILESAPI_NAME `
   --dapr-components './tilesapi-components.yaml' `
   --secrets "servicebusconnectionstring=$SERVICEBUS_CONNECTIONSTRING,tiledbconnectionstring=$TILE_DB_CONNECTIONSTRING" `
-  --environment-variables "tiledbconnectionstring=secretref:tiledbconnectionstring"
+  --environment-variables "tiledbconnectionstring=secretref:tiledbconnectionstring" `
   | ConvertFrom-Json
 Write-Output $TILESAPI_APP.ID
 Write-Output $TILESAPI_APP.configuration.ingress.fqdn
@@ -137,6 +148,7 @@ $TILEPROCESSOR_APP = az containerapp create `
   --dapr-app-port 80 `
   --dapr-app-id $TILEPROCESSOR_NAME `
   --dapr-components './tileprocessor-components.yaml' `
-  --secrets "servicebusconnectionstring=$SERVICEBUS_CONNECTIONSTRING,storage-account-name=${STORAGE_ACCOUNT_NAME},storage-account-key=${STORAGE_ACCOUNT_KEY}"
+  --secrets "servicebusconnectionstring=$SERVICEBUS_CONNECTIONSTRING,storage-account-name=${STORAGE_ACCOUNT_NAME},storage-account-key=${STORAGE_ACCOUNT_KEY}" `
+  | ConvertFrom-Json
 
-  Write-Output $TILEPROCESSOR_APP.ID
+Write-Output $TILEPROCESSOR_APP.ID
