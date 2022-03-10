2fb8309f1ff4c344b6890120d021c748f346ccc5
Ben Williams <ben@negativeeddy.com>
2022-03-09 21:37:09 -0600

    add tiles db schema

---
+++ deploy/sql/tiles.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Tiles] (
+    [Id]       INT            IDENTITY (1, 1) NOT NULL,
+    [Source]   NVARCHAR (MAX) NOT NULL,
+    [SourceId] NVARCHAR (MAX) NOT NULL,
+    [Width]    INT            NULL,
+    [Height]   INT            NULL,
+    [AverageR] TINYINT        NULL,
+    [AverageG] TINYINT        NULL,
+    [AverageB] TINYINT        NULL,
+    [Date]     DATETIME2 (7)  NULL,
+    [Aspect]   REAL           NULL
+);
+
+
