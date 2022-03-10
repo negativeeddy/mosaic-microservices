7fdaa585e2f1916084b98a3b96b39a04149bdad0
Ben Williams <ben@negativeeddy.com>
2022-03-09 21:26:23 -0600

    remove unused scripts

--- deploy/deploy.ps1
+++
@@ -1,2 +0,0 @@
-./deploy_container.ps1 -APP_NAME "mosaic-frontend" -IMAGE_NAME 'mosaicprod.azurecr.io/mosaicfrontend:latest'
-./deploy_container.ps1 -APP_NAME "tilesapi" -IMAGE_NAME 'mosaicprod.azurecr.io/mosaictilesapi:latest'
