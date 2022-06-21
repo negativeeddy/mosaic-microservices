91e89a2a8d56a2c78bcc27d878b3ad3eec377967
Ben Williams <ben@negativeeddy.com>
2022-06-21 13:32:40 -0500

    remove old color info

--- src/Mosaic.TilesApi/Data/TileEntity.cs
+++ src/Mosaic.TilesApi/Data/TileEntity.cs
@@ -9,20 +9,14 @@ public class TileEntity
     [Key]
     public int Id { get; set; }
     [Required]
-    public string Source { get; set; }
+    public string Source { get; set; } = null!;
     [Required]
-    public string SourceId { get; set; }
+    public string SourceId { get; set; } = null!;
     [Required]
-    public string SourceData { get; set; }
+    public string SourceData { get; set; } = null!;
     public int? Width { get; set; }
     public int? Height { get; set; }
     public Point? Average { get; set; }
-    [NotMapped]
-    public byte? AverageR { get; set; }
-    [NotMapped]
-    public byte? AverageG { get; set; }
-    [NotMapped]
-    public byte? AverageB { get; set; }
     public DateTime? Date { get; set; }
     public float? Aspect { get; set; }
 }
