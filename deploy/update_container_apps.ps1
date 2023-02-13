7b4bf4a6ad6162219ffcd43952e8965600b0c390
Ben Williams <ben@negativeeddy.com>
2023-02-13 09:54:15 -0600

    convert bicep to json

--- deploy/update_container_apps.ps1
+++ deploy/update_container_apps.ps1
@@ -12,10 +12,12 @@ Param(
 
 $RESOURCE_GROUP_NAME = "$RESOURCE_GROUP_BASE-rg-$STAGE"
 
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-frontend-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/frontend:$IMAGE_TAG"
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-tilesapi-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/tilesapi:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-frontend-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/frontend:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-tilesapi-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/tilesapi:$IMAGE_TAG"
 az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-tilesactors-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/tilesactors:$IMAGE_TAG"
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-tileprocessor-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/tileprocessor:$IMAGE_TAG"
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-mosaicapi-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/mosaicapi:$IMAGE_TAG"
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-mosaicgenerator-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/mosaicgenerator:$IMAGE_TAG"
-az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-apigateway-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/apigateway:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-tileprocessor-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/tileprocessor:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-mosaicapi-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/mosaicapi:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-mosaicgenerator-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/mosaicgenerator:$IMAGE_TAG"
+# az containerapp update --resource-group $RESOURCE_GROUP_NAME --name "$APP_NAME-apigateway-ca-$STAGE" --image "$CONTAINER_REGISTRY/mosaic/apigateway:$IMAGE_TAG"
+
+az containerapp revision copy --resource-group $RESOURCE_GROUP --name "$APP_NAME-tilesactors-ca-$STAGE"
